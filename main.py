5fbdfbf76b90636b38bfafc3bc0a7675c2d99c77
Dylan Ritchings <dylan.ritchings@metoffice.gov.uk>
2023-03-22 13:29:06 +0000

    BGIP-12 # added drop counter functionality

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-def create_board(w, h):
+def create_board(w: int, h: int):
     return [[" " for x in range(w)] for y in range(h)]
 
 
-def print_border_row(length):
-    for x in range(length + 1):
+def print_border_row(length: int):
+    for x in range(length):
         print("---", end="")
     print("-")
 
 
-def print_board(board):
+def print_board(board: list):
     for row in board:
-        print_border_row(len(board))
+        print_border_row(len(board[0]))
 
         for cell in row:
             print("|", cell, end="")
         print("|")
 
-    print_border_row(len(board))
+    print_border_row(len(board[0]))
+
+
+def insert_counter(board: list, position: int, counter: str):
+    for idx, row in enumerate(reversed(board)):
+        # Check if empty
+        if row[position] == " ":
+            row[position] = counter
+            break
+        # Check if top row
+        if idx == len(board)-1:
+            # do something
+            print("error")
+
+    return board
+
 
 
 if __name__ == '__main__':
-    board = create_board(7, 6)
+    board = create_board(7, 5)
+    print_board(board)
+    board = insert_counter(board, 2, "x")
+    board = insert_counter(board, 2, "x")
     print_board(board)
