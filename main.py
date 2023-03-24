6d54e6fd7b11959dd20b5fadc5fe2cc656ee5842
Dylan Ritchings <dylan.ritchings@metoffice.gov.uk>
2023-03-24 13:21:49 +0000

    added tests, win checker and play game functions

--- main.py
+++ main.py
@@ -1,42 +1,120 @@
-def create_board(w: int, h: int):
-    return [[" " for x in range(w)] for y in range(h)]
+import itertools
 
+class Board:
+    def __init__(self, width: int, height: int):
+        self.board = [[" " for x in range(width)] for y in range(height)]
 
-def print_border_row(length: int):
-    for x in range(length):
-        print("---", end="")
-    print("-")
+    def __print_border_row(self, length: int):
+        for x in range(length):
+            print("---", end="")
+        print("--")
 
+    def print(self):
+        print(end=" ")
+        for count in range(len(self.board) + 1):
+            print("|", count, end="")
 
-def print_board(board: list):
-    for row in board:
-        print_border_row(len(board[0]))
-
-        for cell in row:
-            print("|", cell, end="")
         print("|")
+        for count, row in enumerate(self.board):
+            self.__print_border_row(len(self.board[0]))
+            print(count, end="")
 
-    print_border_row(len(board[0]))
+            for cell in row:
+                print("|", cell, end="")
+            print("|")
+
+        self.__print_border_row(len(self.board[0]))
+        print()
+
+    def get_board(self) -> list:
+        return self.board
+
+    def insert_counter(self, position: int, counter: str) -> list:
+        for idx, row in enumerate(reversed(self.board)):
+            # Check if empty
+            if row[position] == " ":
+                row[position] = counter
+                break
+            # Check if top row
+            if idx == len(self.board) - 1:
+                # do something
+                print("error")
+
+        return self.board
+
+    def __check_row(self, length: int, counter: str, row_idx: int) -> bool:
+        line = ''.join(self.board[row_idx])
+        if counter * length in line:
+            return True
+
+    def __check_col(self, length: int, counter: str, position: int) -> bool:
+        line = ''.join(row[position] for row in self.board)
+        if counter * length in line:
+            return True
+
+    def __get_row_from_col(self, counter: str, col_idx: int) -> int:
+        for row_idx, row in enumerate(self.board):
+            if row[col_idx] == counter:
+                return row_idx
+
+    def __check_ngtv_line(self, length: int, counter: str, col_idx: int, row_idx: int) -> bool:
+        line = ""
+        for n in range(-length, length, 1):
+            if not (col_idx + n < 0 or col_idx + n >= len(self.board[0]) or
+                    row_idx + n < 0 or row_idx + n >= len(self.board)):
+                line += (self.board[row_idx + n][col_idx + n])
+
+        if counter * length in line:
+            return True
+
+    def __check_pos_line(self, length: int, counter: str, col_idx: int, row_idx: int) -> bool:
+        line = ""
+        for n in range(-length, length, 1):
+            if not (col_idx + n < 0 or col_idx + n >= len(self.board[0]) or
+                    row_idx - n < 0 or row_idx - n >= len(self.board)):
+                line += (self.board[row_idx - n][col_idx + n])
+
+        if counter * length in line:
+            return True
+
+    def check_win(self, length: int, counter: str, col_idx: int) -> bool:
+        row_idx = self.__get_row_from_col(counter, col_idx)
+
+        if self.__check_col(length, counter, col_idx):
+            return True
+        elif self.__check_row(length, counter, row_idx):
+            return True
+        elif self.__check_ngtv_line(length, counter, col_idx, row_idx):
+            return True
+        elif self.__check_pos_line(length, counter, col_idx, row_idx):
+            return True
+        else:
+            return False
 
 
-def insert_counter(board: list, position: int, counter: str):
-    for idx, row in enumerate(reversed(board)):
-        # Check if empty
-        if row[position] == " ":
-            row[position] = counter
+def turn(board: Board, counter: str, win_length: int) -> bool:
+    counter_pos = int(input(f"Player {counter} enter column number: "))
+    print()
+    board.insert_counter(counter_pos, counter)
+    board.print()
+
+    if board.check_win(win_length, counter, counter_pos):
+        print(f"Player {counter} Wins!")
+        return True
+    else:
+        return False
+
+
+def play_game():
+    win_length = 4
+    counters = itertools.cycle(["o", "x"])
+    board = Board(7, 6)
+    board.print()
+
+    while True:
+        if turn(board, next(counters), win_length):
             break
-        # Check if top row
-        if idx == len(board)-1:
-            # do something
-            print("error")
-
-    return board
-
 
 
 if __name__ == '__main__':
-    board = create_board(7, 5)
-    print_board(board)
-    board = insert_counter(board, 2, "x")
-    board = insert_counter(board, 2, "x")
-    print_board(board)
+    play_game()
--- test.py
+++ test.py
@@ -1,11 +1,73 @@
-import unittest
-import main
+import time
+
+from main import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+def test_board_add_counter(board):
+    board.insert_counter(0, "o")
+    assert board.get_board()[-1][0] == "o"
+
+
+# def test_board_add_counter_over(board):
+#     board.insert_counter(10, "o")
+
+
+def test_check_win_horizontal(board):
+    board.insert_counter(0, "o")
+    board.insert_counter(1, "o")
+    board.insert_counter(2, "o")
+    board.insert_counter(3, "o")
+
+    assert board.check_win(4, "o", 2) is True
+
+def test_check_win_vertical(board):
+    board.insert_counter(0, "o")
+    board.insert_counter(0, "o")
+    board.insert_counter(0, "o")
+    board.insert_counter(0, "o")
+    assert board.check_win(4, "o", 0) is True
+
+
+def test_check_win_negative_gradient(board):
+    board.insert_counter(2, "x")
+    board.insert_counter(1, "x")
+    board.insert_counter(1, "x")
+    board.insert_counter(0, "x")
+    board.insert_counter(0, "x")
+    board.insert_counter(0, "x")
+    board.insert_counter(0, "o")
+    board.insert_counter(1, "o")
+    board.insert_counter(2, "o")
+    board.insert_counter(3, "o")
+    board.print()
+    assert board.check_win(4, "o", 1) is True
+
+def test_check_win_positive_gradient(board):
+    board.insert_counter(1, "x")
+    board.insert_counter(2, "x")
+    board.insert_counter(2, "x")
+    board.insert_counter(3, "x")
+    board.insert_counter(3, "x")
+    board.insert_counter(3, "x")
+    board.insert_counter(0, "o")
+    board.insert_counter(1, "o")
+    board.insert_counter(2, "o")
+    board.insert_counter(3, "o")
+
+    assert board.check_win(4, "o", 1) is True
+
 
 
 if __name__ == '__main__':
-    unittest.main()
+
+    board = Board(7,6)
+    test_board_add_counter(board)
+    board = Board(7,6)
+    test_check_win_horizontal(board)
+    board = Board(7,6)
+    test_check_win_vertical(board)
+    board = Board(7,6)
+    test_check_win_negative_gradient(board)
+    board = Board(7,6)
+    test_check_win_positive_gradient(board)
+
